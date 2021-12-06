16553bf009abec5edb9b0ed6c826e3bb6207203a
shoebham <25881429+shoebham@users.noreply.github.com>
2021-12-06 11:01:52 +0530

    Day-3 Part-2

--- advent-of-code-2021-day-3.cpp
+++ advent-of-code-2021-day-3.cpp
@@ -24,6 +24,7 @@ void init_code(){
     
 }
 
+// part 1
 void solve(vector<string> arr,int size)
 {
 	vector<int> count_one(arr[0].length(),0);
@@ -60,6 +61,118 @@ void solve(vector<string> arr,int size)
 	cout<<gamma*epsilon<<endl;
 }
 
+void solve2(vector<string> arr,int size)
+{
+	map<string,int> count_one;
+	map<string,int> count_zero;
+	vector<string> one;
+	vector<string> zero;
+	int c0=0;
+	int c1=0;
+	vector<string> newarr=arr;
+	string oxy;
+	string co2;
+		// oxygen generator rating
+		rep(j,arr[0].length())
+		{
+			one.clear();
+			c1=0;
+			c0=0;
+			zero.clear();
+			size=arr.size();
+			rep(i,size)
+			{
+				if(arr[i][j]=='1')
+				{
+					one.push_back(arr[i]);		
+					c1++;
+				}
+				else
+				{
+					zero.push_back(arr[i]);
+					c0++;
+				}
+			}
+			// cout<<"j "<<j<<" ";
+			// cout<<"c1 "<<c1<<" c0 "<<c0<<endl;
+			// for(auto x:arr)cout<<x<<"\n";
+			// for(auto x:one)cout<<x<<"\n";
+			if(c1>=c0)
+			{
+				arr.clear();
+				arr.resize(one.size());
+				rep(k,one.size())
+				{
+					arr[k]=one[k];
+				}
+			}
+			else
+			{
+				arr.clear();
+				arr.resize(zero.size());
+				rep(k,zero.size())
+				{
+					arr[k]=zero[k];
+				}	
+			}
+		}
+		// for(auto x:arr)cout<<x<<"\n";
+		oxy=arr[0];
+		one.clear();
+		zero.clear();
+		c0=0;
+		c1=0;
+		// co2 scrubber rating 
+		rep(j,newarr[0].length())
+		{
+			one.clear();
+			c1=0;
+			c0=0;
+			zero.clear();
+			size=newarr.size();
+			rep(i,size)
+			{
+				if(newarr[i][j]=='1')
+				{
+					one.push_back(newarr[i]);		
+					c1++;
+				}
+				else
+				{
+					zero.push_back(newarr[i]);
+					c0++;
+				}
+			}
+			// cout<<"j "<<j<<" ";
+			// cout<<"c1 "<<c1<<" c0 "<<c0<<endl;
+			// for(auto x:arr)cout<<x<<"\n";
+			// for(auto x:one)cout<<x<<"\n";
+			if(c1>=c0)
+			{
+				newarr.clear();
+				newarr.resize(zero.size());
+				rep(k,zero.size())
+				{
+					newarr[k]=zero[k];
+				}
+			}
+			else
+			{
+				newarr.clear();
+				newarr.resize(one.size());
+				rep(k,one.size())
+				{
+					newarr[k]=one[k];
+				}	
+			}
+		}
+		co2=newarr[0];
+		// cout<<oxy<< " "<<co2;
+		int oxygen=stoi(oxy,0,2);
+		int carbon=stoi(co2,0,2);
+
+		cout<<oxygen*carbon;
+}
 int main()
 {
     init_code();
@@ -77,6 +190,6 @@ int main()
     	// cout<<num<<endl;
     	// arr.push_back(num);
     }
-    solve(arr,size);
+    solve2(arr,size);
     return 0;
 }
